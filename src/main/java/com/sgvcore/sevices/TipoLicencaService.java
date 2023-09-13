8cd474029e199be1ea09ee82d7b76bf98aeb292d
Mardel Juma <mardel.mjj@gmail.com>
2023-09-13 09:50:01 +0200

    Tipos de Licenca

--- src/main/java/com/sgvcore/sevices/TipoLicencaService.java
+++ src/main/java/com/sgvcore/sevices/TipoLicencaService.java
@@ -1,5 +1,6 @@
 package com.sgvcore.sevices;
 
+import com.sgvcore.DTOs.tipoLicencaDTOs.TipoLicencaRespostaDTO;
 import com.sgvcore.Model.TipopLicenca;
 import com.sgvcore.exceptions.ModelNotFound;
 import com.sgvcore.repository.TipoLicencaRepo;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TipoLicencaService {
@@ -24,6 +26,10 @@ public class TipoLicencaService {
 
     public TipopLicenca buscarTipoLicencaPorId(Long id) throws ModelNotFound {
         return tipoLicencaRepo.findById(id).orElseThrow(() -> new ModelNotFound("Tipo de licenca nao encontrada"));
+    }
+
+    public List<TipoLicencaRespostaDTO>listarTiposLicencaResposta(){
+        return tipoLicencaRepo.findAll().stream().map(tipopLicenca -> new TipoLicencaRespostaDTO(tipopLicenca)).collect(Collectors.toList());
     }
 
 }
