84f223cde38a4b2ba1cd21195d48b2598006209f
IsraelMatusse <64605974+IsraelMatusse@users.noreply.github.com>
2023-09-15 14:38:16 +0200

    quick fix

--- src/main/java/com/sgvcore/config/SwaggerConfig.java
+++ src/main/java/com/sgvcore/config/SwaggerConfig.java
@@ -1,14 +1,5 @@
 package com.sgvcore.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 public class SwaggerConfig {
